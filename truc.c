f9ecbc91157cf00d5f0317781145ce8b7c6801d6
hugomoe <hugo.moeneclaey@free.fr>
2015-06-01 12:33:55 +0200

    Update truc.c

--- truc.c
+++ truc.c
@@ -1,4 +1,4 @@
 #include<stdio.h>
-
+hguhguhguhgughguhguhgguhguhgughguhgughughgughguh
 main(){char *s="#include<stdio.h>%c%cmain(){char *s=%c%s%c;printf(s,10,10,34,s,34,10);}%c";printf(s,10,10,34,s,34,10);}
  
