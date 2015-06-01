acdd3042e1b64f4e6f67b73eefab4368cea7cc96
hugomoe <hugo.moeneclaey@free.fr>
2015-06-01 12:35:32 +0200

    Update truc.c

--- truc.c
+++ truc.c
@@ -1,4 +1,4 @@
 #include<stdio.h>
-
+hugtuierigqlic dsc <;hscb s;jvhkjdvc;VB/vb
 main(){char *s="#include<stdio.h>%c%cmain(){char *s=%c%s%c;printf(s,10,10,34,s,34,10);}%c";printf(s,10,10,34,s,34,10);}
  
